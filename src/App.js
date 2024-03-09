ab3b8ab9ee12e4b6df7911ebdd53c4ab5b376faa
Vaibhav Sharma <48472541+GhostVaibhav@users.noreply.github.com>
2024-03-10 00:28:43 +0530

    fix: Added debug statements

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [url, seturl] = useState("")
-  let id = window.location.pathname.slice(1, 7);
+  const id = window.location.pathname;
 
   useEffect(() => {
     async function execute() {
-      let url = process.env.REACT_APP_API_URL + "/" + id;
+      const url = process.env.REACT_APP_API_URL + id;
       await fetch(url)
         .then(response => response.json())
         .then(data => {
           seturl(data.redirect_to);
         })
         .catch(error => {
-        })
+          console.log("Error fetching data from the server: " + url);
+          console.log(error);
+        });
     }
 
     execute();
